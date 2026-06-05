ee493b0cfa9b8976b918b7ae7fa97e5ba3088a88
kunai <neni23@mail.ru>
2026-06-05 15:48:08 +0300

    added v2 with first version

--- main.go
+++ main.go
@@ -26,29 +26,94 @@ func (u *User) Withdraw(sum float64) error {
 	return nil
 }
 
+type Transaction struct {
+	FromID string
+	ToID   string
+	Amount float64
+}
+
+type PaymentSystem struct {
+	Users        map[string]*User
+	Transactions []Transaction
+}
+
+func NewPaymentSystem() *PaymentSystem {
+	return &PaymentSystem{
+		Users:        make(map[string]*User),
+		Transactions: make([]Transaction, 5, 10),
+	}
+}
+
+func (ps *PaymentSystem) AddUser(user *User) {
+	ps.Users[user.ID] = user
+	fmt.Printf("User with ID %s added in PaymentSystem!\n", user.ID)
+}
+
+func (ps *PaymentSystem) AddTransaction(transaction *Transaction) {
+	ps.Transactions = append(ps.Transactions, *transaction)
+	fmt.Printf("Transaction from %s to %s users added in queue\n", transaction.FromID, transaction.ToID)
+}
+
+func (ps *PaymentSystem) ProcessingTransactions(transaction Transaction) error {
+	if _, ok := ps.Users[transaction.FromID]; !ok {
+		errString := fmt.Sprintf("user with ID %s not found in payment system", transaction.FromID)
+		return errors.New(errString)
+	}
+	if _, ok := ps.Users[transaction.ToID]; !ok {
+		errString := fmt.Sprintf("user with ID %s not found in payment system", transaction.ToID)
+		return errors.New(errString)
+	}
+	if err := ps.Users[transaction.FromID].Withdraw(transaction.Amount); err != nil {
+		return err
+	}
+	ps.Users[transaction.ToID].Deposit(transaction.Amount)
+
+	return nil
+}
+
 func main() {
+	paymentSystem := NewPaymentSystem()
 	user1 := &User{
 		ID:      "user1",
 		Name:    "John",
 		Balance: 100,
 	}
+	paymentSystem.AddUser(user1)
 	user2 := &User{
 		ID:      "user2",
 		Name:    "Regina",
 		Balance: 68.5,
 	}
-
-	user1.Deposit(50.5)
-	user2.Deposit(10.10)
+	paymentSystem.AddUser(user2)
+	transaction1 := &Transaction{
+		FromID: user1.ID,
+		ToID:   user2.ID,
+		Amount: 50.5,
+	}
+	paymentSystem.AddTransaction(transaction1)
+	transaction2 := &Transaction{
+		FromID: user2.ID,
+		ToID:   user1.ID,
+		Amount: 18,
+	}
+	paymentSystem.AddTransaction(transaction2)
 	fmt.Println(user1.Balance)
 	fmt.Println(user2.Balance)
 
-	if err := user1.Withdraw(300); err != nil {
-		fmt.Printf("Error for %s: %s\n", user1.Name, err)
-	}
-	if err := user2.Withdraw(5.19); err != nil {
-		fmt.Printf("Error for %s: %s\n", user2.Name, err)
+	for i := range paymentSystem.Transactions {
+		fmt.Printf("Transaction from %s to %s. Sum: %.2f", paymentSystem.Transactions[i].FromID, paymentSystem.Transactions[i].ToID, paymentSystem.Transactions[i].Amount)
+		fmt.Println(user1.Balance)
+		fmt.Println(user2.Balance)
 	}
 	fmt.Println(user1.Balance)
 	fmt.Println(user2.Balance)
+
+	// if err := user1.Withdraw(300); err != nil {
+	// 	fmt.Printf("Error for %s: %s\n", user1.Name, err)
+	// }
+	// if err := user2.Withdraw(5.19); err != nil {
+	// 	fmt.Printf("Error for %s: %s\n", user2.Name, err)
+	// }
+	// fmt.Println(user1.Balance)
+	// fmt.Println(user2.Balance)
 }
